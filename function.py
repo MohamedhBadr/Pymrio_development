5db5ccd59fec8b4d5f0b51121f8cac9f22fe5321
Mohamed Badr <62759252+MohamedhBadr@users.noreply.github.com>
2021-09-15 13:29:17 +0200

    Update function.py
    Organized the script in a cleaner format.

--- function.py
+++ function.py
@@ -1,22 +1,20 @@
-#import libraries (test dataset)
+#import libraries needed for function 
+import numpy as np
 import pymrio
-test_mrio = pymrio.load_test()
 
-test_mrio.emissions.D_cba
 
-#Calculate missing tables
-test_mrio.calc_all()
-
-df = test_mrio.emissions.D_cba
-df.head()
-
-#create function 
+#create function (a more efficient way of writing the function?)
 
 def get_emission(region, emission_type):
-    import numpy as np
+    test_mrio = pymrio.load_test()
+    test_mrio.calc_all()
+    df = test_mrio.emissions.D_cba
     df1= df[np.in1d(df.index.get_level_values(1), [emission_type])]
     df2 = df1[region]
     print(df2)
 
-get_emission('reg1', 'air')
+#test
+get_emission('reg2', 'water')
 
+
+#more detail can be added by including a sector sub category/ filter
