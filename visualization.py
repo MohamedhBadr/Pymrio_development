14168ec7c54445f2e7bcfdd18e0ac6466b799da5
Mohamed Badr <62759252+MohamedhBadr@users.noreply.github.com>
2021-09-17 11:17:47 +0200

    Visualization
    Here I make a script that explores the possibility of visualizing the returned results from the get_emissions function.

---
+++ visualization.py
@@ -0,0 +1,31 @@
+
+#%%
+import pandas as pd
+
+#import libraries needed for function 
+import numpy as np
+import pymrio
+import matplotlib as plt
+
+#create function (a more efficient way of writing the function?)
+
+def get_emission(region, emission_type):
+    test_mrio = pymrio.load_test()
+    test_mrio.calc_all()
+    df = test_mrio.emissions.D_cba
+    df1= df[np.in1d(df.index.get_level_values(1), [emission_type])]
+    df2 = df1[region]
+    return(df2)
+
+#test
+df3 = get_emission('reg2', 'water')
+
+#output_file = input("Save file as \n")
+#df3.to_csv(output_file + '.tsv', sep ='\t')
+
+#visualize the output of the function 
+df3.plot()
+
+#import matplotlib as plt 
+
+# %%
