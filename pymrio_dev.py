8822d983c95dfd4f5694a0ea4e849c3818daf34d
Mohamed Badr <62759252+MohamedhBadr@users.noreply.github.com>
2021-10-07 13:41:14 +0200

    Add files via upload

---
+++ pymrio_dev.py
@@ -0,0 +1,91 @@
+# %%
+#import libraries
+import pymrio
+import pandas as pd
+import numpy as np 
+from pathlib import Path
+import matplotlib as plt
+
+##Import and Proccess EXIO 2017##
+
+
+# %%
+#import Exiobase 2017
+
+io_path_2017 = Path("./IOT_2017_pxp.zip")
+io_2017 = pymrio.parse_exiobase3(io_path_2017)
+# %%
+#calculate all and get d_cba
+
+gwp = "GHG emissions (GWP100) | Problem oriented approach: baseline (CML, 2001) | GWP100 (IPCC, 2007)"
+io_2017.calc_all()
+d_cba_2017 = io_2017.impacts.D_cba_reg.loc[gwp, :]
+
+
+
+#Import and Proccess EXIO 2015
+io_path_2015 = Path("./IOT_2015_ixi.zip")
+io_2015 = pymrio.parse_exiobase3(io_path_2015)
+io_2015.calc_all()
+d_cba_2015 = io_2015.impacts.D_cba_reg.loc[gwp, :]
+
+# %%
+#Import and Proccess EXIO 2016
+io_path_2016 = Path("./IOT_2016_ixi.zip")
+io_2016 = pymrio.parse_exiobase3(io_path_2016)
+io_2016.calc_all()
+d_cba_2016 = io_2016.impacts.D_cba_reg.loc[gwp, :]
+
+# %%
+#merge into one dataframe 
+
+io_2015_2016 = pd.merge(d_cba_2015, d_cba_2016, left_index=True, right_index=True)
+io_2015__2017 = pd.merge(io_2015_2016, d_cba_2017, left_index=True, right_index=True)
+io_2015__2017
+
+# %%
+#Rename columns 
+df = io_2015__2017
+df = df.rename(columns={'GHG emissions (GWP100) | Problem oriented approach: baseline (CML, 2001) | GWP100 (IPCC, 2007)_x': '2015'})
+df = df.rename(columns={'GHG emissions (GWP100) | Problem oriented approach: baseline (CML, 2001) | GWP100 (IPCC, 2007)_y': '2016'})
+df = df.rename(columns={'GHG emissions (GWP100) | Problem oriented approach: baseline (CML, 2001) | GWP100 (IPCC, 2007)': '2017'})
+df
+# %%
+#define function that gets country and emissions 
+def get_time_series(country_code):
+    df2 = df[country_code]
+    ax = df2.unstack(level=1).plot(kind='bar', subplots=False, rot=0, figsize=(10, 5), layout=(4, 6))
+    plt.title(emission_type + '_pollution_in_' +region)
+    plt.tight_layout()
+# %%
+#subset data
+df2 = df.loc[ 'NL' , : ]
+df2
+# %%
+#Write function to return values 
+def get_time_series(country):
+    df2 = df.loc[ country , : ]
+    df2
+    return df2
+# %%
+#test function (appears to work fine)
+df_russia = get_time_series('RU')
+
+# %%
+df_russia = df_russia.reset_index()
+df_russia
+# %%
+plt.pyplot.title('RU')
+plt.pyplot.bar('index', 'RU',width=0.8, bottom=None, align='center', data=df_russia)
+# %%
+#Write a visualization function
+def get_ts_viz (country_code):
+    new_df = get_time_series(country_code)
+    new_df = new_df.reset_index()
+    plt.pyplot.title(country_code)
+    plt.pyplot.bar('index', country_code, width=0.8, bottom=None, align='center', data=new_df)
+
+# %%
+#Test visualization function 
+get_ts_viz('NL')
+# %%
